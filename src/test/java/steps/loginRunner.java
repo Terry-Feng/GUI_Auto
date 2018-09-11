59f8d5af894ca9515bd5440ec5cd5bd72251477d
Terry Feng <terry.jxfeng@gmail.com>
2018-09-11 18:30:14 +1200

    update test runner
    update test runner and pom

--- src/test/java/steps/loginRunner.java
+++ src/test/java/steps/loginRunner.java
@@ -1,10 +1,10 @@
-package runner;
+package steps;
 
 import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features = "src/test/resources/features/login.feature")
+@CucumberOptions(features="src/test/resources/features/")
 public class loginRunner {
 }
